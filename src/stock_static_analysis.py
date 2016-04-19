bfe03168ebe793458cf89b05a90e7b4ee44fa992
alex <salexln@gmail.com>
2016-04-19 20:31:52 +0100

    work in progress

--- src/stock_static_analysis.py
+++ src/stock_static_analysis.py
@@ -1,19 +1,27 @@
 import argh
+from argh.decorators import arg
+
+
+
+
+def read_file(path):
+	with open(path, 'r') as f:
+		lines = f.readlines()
+
+		for line in lines:
+			print line
 
 
 
 
 
 
-
-
-
-
-
-
-
-
-
+@arg('--data', required = True, help='CSV file with stock data')
+def run_stock_analysis(**kwargs):
+	path = kwargs['data']
+	read_file(path)
 
 if __name__ == '__main__':
-	print 'aa'
+	p = argh.ArghParser()
+	argh.set_default_command(p, run_stock_analysis)
+	p.dispatch()
